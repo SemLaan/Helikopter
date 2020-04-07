4fade0941cc25a306b7de42c8399e793bdb722f5
sem laan <semlaan@dds.nl>
2020-04-07 19:55:15 +0200

    Improved the camera

---
+++ Helikopter/Assets/Scripts/Plane/PlaneCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneCamera : MonoBehaviour
+{
+
+    [SerializeField] private new Transform camera = null;
+    [SerializeField] private float verticalDistance = 1f;
+    [SerializeField] private float horizontalDistance = 1f;
+    private new Rigidbody rigidbody;
+
+
+
+    private void Awake()
+    {
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    private void Update()
+    {
+
+        // Updating camera position
+        Vector3 horizontalPlayerDistance = -rigidbody.velocity;
+        horizontalPlayerDistance.y = 0;
+        horizontalPlayerDistance = horizontalPlayerDistance.normalized * horizontalDistance;
+        camera.position = transform.position + horizontalPlayerDistance + (Vector3.up * verticalDistance);
+
+        // Updating camera rotation
+        camera.LookAt(transform);
+    }
+}
