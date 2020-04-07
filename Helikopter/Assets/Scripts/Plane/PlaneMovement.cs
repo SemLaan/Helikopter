9e7044bc5d7677fdd1b77e4f175473b37c8f14c4
sem laan <semlaan@dds.nl>
2020-04-07 13:03:23 +0200

    Input detection and going forward

---
+++ Helikopter/Assets/Scripts/Plane/PlaneMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+
+    [SerializeField] private float motorPower = 1f;
+
+    private Vector2 leftJoystick, rightJoystick;
+    private Controls controls;
+
+    private new Rigidbody rigidbody;
+
+
+
+    private void Awake()
+    {
+
+        rigidbody = GetComponent<Rigidbody>();
+        controls = new Controls();
+        controls.Enable();
+    }
+
+
+
+    private void FixedUpdate()
+    {
+
+        Vector2 leftJoystick = controls.Gameplay.left.ReadValue<Vector2>();
+        Vector2 rightJoystick = controls.Gameplay.right.ReadValue<Vector2>();
+
+        if (leftJoystick.y > 0)
+            rigidbody.AddRelativeForce(new Vector3(0, 0, motorPower * leftJoystick.y));
+    }
+}
