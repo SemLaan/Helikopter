d8537460a89084bd12cbb83012b5508b9ba05619
sem laan <semlaan@dds.nl>
2020-04-07 15:39:15 +0200

    Added lift to the plane

--- Helikopter/Assets/Scripts/Plane/PlaneMovement.cs
+++ Helikopter/Assets/Scripts/Plane/PlaneMovement.cs
@@ -6,6 +6,7 @@ public class PlaneMovement : MonoBehaviour
 {
 
     [SerializeField] private float motorPower = 1f;
+    [SerializeField] private float liftCoefficient = 1f;
 
     private Vector2 leftJoystick, rightJoystick;
     private Controls controls;
@@ -26,11 +27,20 @@ public class PlaneMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-
+        // Collecting input
         Vector2 leftJoystick = controls.Gameplay.left.ReadValue<Vector2>();
         Vector2 rightJoystick = controls.Gameplay.right.ReadValue<Vector2>();
 
+        // Moving if the player is pushing the left joystick
         if (leftJoystick.y > 0)
             rigidbody.AddRelativeForce(new Vector3(0, 0, motorPower * leftJoystick.y));
+
+        // Adding lift force
+        rigidbody.AddForce(new Vector3(0, Lift));
+
+
+    private float Lift
+    {
+        get => (liftCoefficient * Mathf.Pow(rigidbody.velocity.magnitude, 2f)) / 2f; // TODO: wing area
     }
 }
